a766fba025751573ec7980b16ac3d0a93179137e
Johan <njoubert06@gmail.com>
2021-01-05 17:03:03 +0100

    section 5 done

--- 5-import/script.js
+++ 5-import/script.js
@@ -2,7 +2,28 @@
 
 import {hello} from './functions.js'
 
-hello()
+import {showError} from './functions.js'
 
+import anonymous from './functions.js'
+
+
+
+
+
+hello()
+// showError()
+// anonymous()
 // 5-2 Export
 const [a, b] = [7, 9]
+
+import {sum} from './math.js'
+import {sub} from './math.js'
+import {multiply} from './math.js'
+import {divide} from './math.js'
+import {pow} from './math.js'
+
+console.log(sum(a, b))
+console.log(sub(a, b))
+console.log(multiply(a, b))
+console.log(divide(a, b))
+console.log(pow(a, b))
